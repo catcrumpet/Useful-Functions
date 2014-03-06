c200b179801deb24aaaeaca9a96ca37f989ea9c6
Daniel Chu <catcrumpet@gmail.com>
2014-03-05 21:41:55 -0800

    Added the source

--- convert.magic.R
+++ convert.magic.R
@@ -1,8 +1,11 @@
 # Converts the types for columns of a data frame (obj) into types specified by a vector (types)
 # Returns a data frame
 
+
 convert.magic <- function(obj,types){
     out <- lapply(1:length(obj),FUN = function(i){FUN1 <- switch(types[i],character = as.character,numeric = as.numeric,factor = as.factor); FUN1(obj[,i])})
     names(out) <- colnames(obj)
     as.data.frame(out)
 }
+
+# From "joran" at https://stackoverflow.com/questions/7680959/convert-type-of-multiple-columns-of-a-dataframe-at-once
